7cf4849532b6ebc7537ddddd18c5f94f57de7e3a
RonohP <38761529+RonohP@users.noreply.github.com>
2019-06-22 22:48:48 +0300

    final contact

--- contact.php
+++ contact.php
@@ -1,5 +1,12 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor\autoload.php';
+
+date_default_timezone_set('Africa/Nairobi');
+
 if (isset($_POST['submit'])) {
 
     $from = $_POST['email'];
@@ -7,12 +14,37 @@ if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $message = $_POST['message'];
 
-    $headers = "FROM:" . $from;
+    $mail = new PHPMailer(true);
 
-    $mail = "Name:" . $name . "\n\n" . $message;
-    $to = "testcompany637@gmail.com";
+    $mail->isSMTP();
 
-    if (mail($to, $subject, $mail, $headers)) {
+    //Enable SMTP debugging
+    $mail->SMTPDebug = 2;
+
+    /* Google's SMTP */
+    $mail->Host = 'smtp.gmail.com';
+
+    $mail->Port = 587;
+
+    $mail->SMTPSecure = 'tls';
+
+    $mail->SMTPAuth = TRUE;
+
+    //username and password
+    $mail->Username = 'testcompany637@gmail.com';
+    $mail->Password = 'TestCompany637#';
+
+    $mail->setFrom('' . $from, '' . $name);
+
+    $mail->addReplyTo(''.$from, ''.$name);
+
+    $mail->addAddress('testcompany637@gmail.com', 'TotoLife');
+
+    $mail->Subject = $subject;
+
+    $mail->Body = $message;
+
+    if ($mail->send()) {
         $msg = "Mail sent! Thank you" . " " . $name . ", We appreciate your feedback!";
         echo "<script type='text/javascript'>
 				window.location.href='contact.html';
